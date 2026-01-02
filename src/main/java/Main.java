d4feb43c1cd290505f6621f271a45a5656138b4a
Elena Panaseko <git.user.panasenko-elen@yandex.ru>
2026-01-02 17:11:01 +0300

    changed Main. App counts sum,dif,quoient,multiplication of 2 numbers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         {
-            System.out.println("Введите текст и нажмите <Enter>:");
-            String text = new Scanner(System.in).nextLine();
-            System.out.println("Длинна текста: " + text.length());
+            //ввод чисел
+            System.out.println("Введите первое число:");
+            int firstNumber=new Scanner(System.in).nextInt();
+            System.out.println("Введите второе число:");
+            int secondNumber=new Scanner(System.in).nextInt();
+
+            //вычисления
+            double quoient = (double)firstNumber/secondNumber;
+            int sum = firstNumber+secondNumber;
+            int difference = firstNumber-secondNumber;
+            int multiplication= firstNumber*secondNumber;
+
+            //вывод результатов
+            System.out.println("Сумма:"+sum);
+            System.out.println("Разность:"+difference);
+            System.out.println("Произведение:"+multiplication);
+            System.out.println("Частное:"+quoient);
         }
     }
 }
