70bfef83dc35793dc884e6e1dcf2d40f7f624f97
Elena Panaseko <git.user.panasenko-elen@yandex.ru>
2026-01-08 15:37:15 +0300

    file or directory path parsing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,32 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         {
-            System.out.println("Введите текст и нажмите <Enter>:");
-            String text = new Scanner(System.in).nextLine();
-            System.out.println("Длинна текста: " + text.length());
+            String path = "";
+            int correctPathsNum = 0;
+            int fileNum = 0;
+            String summary = "Номера файлов:";
+            do {
+                System.out.println("Введите путь к файлу");
+                path = new Scanner(System.in).nextLine();
+                File file = new File(path);
+                boolean fileExists = file.exists();
+                boolean isDirectory = file.isDirectory();
+                if (!fileExists && isDirectory) {
+                    System.out.println("Путь указан НЕверно");
+                    //счетчик номеров файлов
+                    // fileNum++;
+                    continue;
+                } else {
+                    correctPathsNum++;
+                    fileNum++;
+                    summary = summary + fileNum + " ";
+                    System.out.println("Путь указан верно. " + summary);
+                    System.out.println("Число корректных файлов:" + correctPathsNum);
+                }
+            } while (true);
         }
     }
 }
