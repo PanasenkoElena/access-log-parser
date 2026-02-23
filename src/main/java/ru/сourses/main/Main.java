ea05852a63b8c2008c716ef58e6669c6cc414297
Elena Panaseko <git.user.panasenko-elen@yandex.ru>
2026-02-23 13:25:16 +0300

    9.yBot,gBot shares

--- src/main/java/ru/сourses/main/Main.java
+++ src/main/java/ru/сourses/main/Main.java
@@ -11,47 +11,77 @@ import java.nio.file.Path;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws LineTooLongException, IOException,LineTooLongException {
+    public static void main(String[] args) throws IOException, LineTooLongException {
         {
             String path = "";
-            int correctPathsNum = 0, fileNum = 0,numOfLines=0, longestLine=0, length=0;
-            int shortestLine=1024;
-            String summary = "Номера файлов:";
-
-                System.out.println("Введите путь к файлу");
-                path = new Scanner(System.in).nextLine();
-                File file = new File(path);
-                boolean fileExists = file.exists();
-            System.out.println("fileExists= "+fileExists);
-                boolean isDirectory = file.isDirectory();
-            System.out.println("isDirectory= "+isDirectory);
+            int length = 0;
+            System.out.println("Введите путь к файлу");
+            path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            System.out.println("fileExists= " + fileExists);
+            boolean isDirectory = file.isDirectory();
+            System.out.println("isDirectory= " + isDirectory);
+            String line, firstBrackets, fragment;
+            float yandexBotQ = 0;
+            String yBot = "YandexBot";
+            float googleBotQ = 0;
+            String gBot = "Googlebot";
+            float otherQ = 0;
             try {
                 FileReader fileReader = new FileReader(file.getAbsoluteFile().getPath());
-                BufferedReader reader =
-                        new BufferedReader(fileReader);
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    if (line.length() > 1024) {
-                        throw new LineTooLongException("Строка имеет длину более 1024 символа. Выполнение программы остановлено");
-                    }
-                    length = line.length();
-                    if (length < shortestLine) {
-                        shortestLine = length;
-                    }
-                    if (length > longestLine) {
-                        longestLine = length;
-                    }}
-                } catch(RuntimeException | IOException ex){}
+                BufferedReader reader = new BufferedReader(fileReader);
 
-            try (var lines = Files.lines(Path.of(path))) {
-                long count = lines.count();
-                System.out.println("Количество строк: " + count);
+                while ((line = reader.readLine()) != null) {
+                    try {
+                        int start = line.indexOf('(');
+                        int end = line.indexOf(')');
+                        if (start != -1 && end != -1 && end > start) {
+                            firstBrackets = line.substring(start + 1, end);
+                            String[] parts = firstBrackets.split(";");
+                            if (parts.length >= 2) {
+                                fragment = parts[1];
+                                fragment = fragment.replaceAll("\\s", "");
+                                boolean contains = fragment.contains("/");
+                                if (contains) {
+                                    fragment = fragment.substring(0, fragment.indexOf("/"));
+                                }
+                                boolean b = fragment.hashCode() == yBot.hashCode() && fragment.equals(yBot);
+                                boolean g = fragment.hashCode() == gBot.hashCode() && fragment.equals(gBot);
+                                if (b) {
+                                    yandexBotQ++;
+                                }
+                                if (g) {
+                                    googleBotQ++;
+                                }
+                                if (b == false && g == false) {
+                                    otherQ++;
+                                }
+                            }
+                        }
+
+                        if (line.length() > 1024) {
+                            throw new LineTooLongException("Строка имеет длину более 1024 символа. Выполнение программы остановлено");
+                        }
+                    } catch (LineTooLongException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                try (var lines = Files.lines(Path.of(path))) {
+                    long count = lines.count();
+                    System.out.println("Количество строк: " + count);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                float yandexBotShare = (yandexBotQ / (yandexBotQ + googleBotQ + otherQ)) * 100;
+                float googleBotShare = (googleBotQ / (yandexBotQ + googleBotQ + otherQ)) * 100;
+                System.out.println("yandexBotShare= " + String.valueOf(yandexBotShare)+"%");
+                System.out.println("googleBotShare= " + String.valueOf(googleBotShare)+"%");
             } catch (IOException e) {
-                e.printStackTrace();
+                throw new RuntimeException(e);
+            } catch (RuntimeException e) {
+                throw new RuntimeException(e);
             }
-            System.out.println("Самая короткая строка имеет количество символов: " + shortestLine);
-            System.out.println("Самая длинная строка имеет количество символов: " + longestLine);
-   // ___________
         }
     }
 }
