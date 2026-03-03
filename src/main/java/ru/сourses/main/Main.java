234ab8e3c84bc396a8a9dac8ed3eaa68e7aba948
Elena Panaseko <git.user.panasenko-elen@yandex.ru>
2026-03-03 22:58:56 +0300

    10.1 Swap arraylist elements

--- src/main/java/ru/сourses/main/Main.java
+++ src/main/java/ru/сourses/main/Main.java
@@ -18,84 +18,20 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException, LineTooLongException {
-        {
-            ArrayList<LogEntry> logEntry = new ArrayList<>();
-            String path = "";
-            System.out.println("Введите путь к файлу");
-            path = new Scanner(System.in).nextLine();
-            File file = new File(path);
-            boolean fileExists = file.exists();
-            System.out.println("fileExists= " + fileExists);
-            boolean isDirectory = file.isDirectory();
-            System.out.println("isDirectory= " + isDirectory);
-            String line;
-            Statistics statistics = new Statistics();
-            long count = 0;
-            int i = 0;
-            try {
-                FileReader fileReader = new FileReader(file.getAbsoluteFile().getPath());
-                BufferedReader reader = new BufferedReader(fileReader);
-                var lines = Files.lines(Path.of(path));
-                count = lines.count();
-                System.out.println("Количество строк: " + count);
-                while ((line = reader.readLine()) != null) {
-                    try {
-                        logEntry.add(new LogEntry(line));
-                      //  System.out.println(logEntry.get(i).toString());
-                        statistics.addEntry(logEntry.get(i));
-                      //  System.out.println("Накопленная статистика "+ statistics.toString());
-                        i++;
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            } catch (IOException e) {
+        { Scanner scanner= new Scanner(System.in);
+            int size = Integer.parseInt(scanner.nextLine());
+            List<Integer> list = new ArrayList<>();
+            int buffer;
+            for (int i = 1; i <= size; i++) {
+                list.add(i);
             }
-            java.text.DecimalFormat df = new java.text.DecimalFormat("###.##");
-            String formattedResult = df.format(statistics.getTrafficRate());
-            System.out.println("Средний трафик в час = "+ formattedResult);
-            System.out.println("Статистика за промежуток от "+statistics.getMinTime()+" до "+statistics.getMaxTime());
-        }
-        return sum;
-    }
 
-    public static List<Double> toNumbers(String string) throws NumberFormatException {
-        String str = string.substring(9);
-        int spaceCount = 0;
-        int i=0;
-        List<String> textNumbers = new ArrayList();
-        do {
-            if (i == str.length() - 1 & str.charAt(i) == ' ') {i++;continue;}
-            if (i <= str.length() - 1 & str.charAt(i) != ' ' & textNumbers.size() == spaceCount) {
-                textNumbers.add(String.valueOf(str.charAt(i)));i++;
-                continue;
-            } else if(i <= str.length() - 1 & str.charAt(i) != ' ' & textNumbers.size() > spaceCount) {
-                String a = textNumbers.get(spaceCount);
-                a += (String.valueOf(str.charAt(i)));
-                textNumbers.set(spaceCount, a); i++; continue;
-            } else if (i <= str.length() - 1 & str.charAt(i) == ' ' & str.charAt(i + 1) != ' ') {
-                spaceCount++; i++; continue;
-            } i++;
-        }
-         while (i < str.length() );
-
-        List<Double> summand = new ArrayList<>();
-        boolean isDouble = true;
-        String checkTextNumber;
-        for (i = 0; i < textNumbers.size(); i++) {
-            checkTextNumber = String.valueOf(textNumbers.get(i));
-            if (isDouble == checkTextNumber.matches("-?\\d+(\\.\\d+)?")) {
-                summand.add(Double.parseDouble(String.valueOf(textNumbers.get(i))));
-                System.out.println(summand.get(summand.size() - 1));
-            } else {
-                summand.add(Double.valueOf(0));
-                System.out.println("В позиции " + i + " было не число");
+            for (int i = 0; i < list.size(); i += 2) {
+                buffer=list.get(i);
+                list.set(i,list.get(i+1));
+                list.set(i+1,buffer);
             }
+            System.out.println(list);
         }
-        return summand;
     }
-
 }
-
-
-
