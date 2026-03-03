08efb7cf4cdcfc980f984f0688d95d48fc106f91
Elena Panaseko <git.user.panasenko-elen@yandex.ru>
2026-03-03 23:17:04 +0300

    10.2 arraylist->array, bubbleSort

--- src/main/java/ru/сourses/main/Main.java
+++ src/main/java/ru/сourses/main/Main.java
@@ -18,20 +18,37 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException, LineTooLongException {
-        { Scanner scanner= new Scanner(System.in);
+        {
+            Scanner scanner = new Scanner(System.in);
             int size = Integer.parseInt(scanner.nextLine());
             List<Integer> list = new ArrayList<>();
             int buffer;
             for (int i = 1; i <= size; i++) {
                 list.add(i);
             }
-
             for (int i = 0; i < list.size(); i += 2) {
-                buffer=list.get(i);
-                list.set(i,list.get(i+1));
-                list.set(i+1,buffer);
+                buffer = list.get(i);
+                list.set(i, list.get(i + 1));
+                list.set(i + 1, buffer);
             }
             System.out.println(list);
+
+            int[] intArray = list.stream().mapToInt(i -> i).toArray();
+
+            bubbleSort(intArray);
+            System.out.println(Arrays.toString(intArray));
+        }
+    }
+
+    public static void bubbleSort(int[] intArray) {
+        for (int i = 0; i < Arrays.stream(intArray).count() - 1; i++) {
+            for (int j = 0; j < Arrays.stream(intArray).count() - i - 1; j++) {
+                if (intArray[j] > intArray[j + 1]) {
+                    int temp = intArray[j];
+                    intArray[j] = intArray[j + 1];
+                    intArray[j + 1] = temp;
+                }
+            }
         }
     }
 }
