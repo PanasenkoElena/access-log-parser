f05809fdd7f5d698d09658943f59c4efcf8c47fb
Elena Panaseko <git.user.panasenko-elen@yandex.ru>
2026-03-05 17:29:59 +0300

    10.getStringFromStream

--- src/main/java/ru/сourses/main/Main.java
+++ src/main/java/ru/сourses/main/Main.java
@@ -13,62 +13,28 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.*;
+import java.util.stream.Stream;
 
 public class Main {
-    public static void main(String[] args) throws IOException, LineTooLongException {
-        {           ArrayList<LogEntry> logEntry = new ArrayList<>();
-            String path = "";
-            System.out.println("Введите путь к файлу");
-            path = new Scanner(System.in).nextLine();
-            File file = new File(path);
-            boolean fileExists = file.exists();
-            System.out.println("fileExists= " + fileExists);
-            boolean isDirectory = file.isDirectory();
-            System.out.println("isDirectory= " + isDirectory);
-            String line;
+    public static void main(String[] args)  {
+        { Scanner scanner = new Scanner(System.in);
+            ArrayList<String> strs = new ArrayList<>();
+            Stream<String> stringStream = Stream.of("стек", "база", "хост", "код", "сайт", "лог", "порт", "файл", "путь", "узел");
+            String output=getStringFromStream(stringStream);
 
-            Statistics statistics = new Statistics();
-            long count = 0;
-            int i = 0;
-            try {
-                FileReader fileReader = new FileReader(file.getAbsoluteFile().getPath());
-                BufferedReader reader = new BufferedReader(fileReader);
-                var lines = Files.lines(Path.of(path));
-                count = lines.count();
-                System.out.println("Количество строк: " + count);
-                while ((line = reader.readLine()) != null) {
-                    try {
-                        logEntry.add(new LogEntry(line));
-                      //  System.out.println(logEntry.get(i).toString());
-                        statistics.addEntry(logEntry.get(i));
-                      //  System.out.println("Накопленная статистика "+ statistics.toString());
-                        i++;
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            } catch (IOException e) {
-            }
-            java.text.DecimalFormat df = new java.text.DecimalFormat("###.##");
-            String formattedResult = df.format(statistics.getTrafficRate());
-            System.out.println("Средний трафик в час = "+ formattedResult);
-            System.out.println("Статистика за промежуток от "+statistics.getMinTime()+" до "+statistics.getMaxTime());
-            statistics.getOsShare();
-            statistics.osShare.forEach((os, share) ->
-                    System.out.printf("ОС: %s, Доля: %.2f%%%n", os, (Double) share * 100));
-            statistics.getBrowserShare();
-            statistics.osShare.forEach((browser, share) ->
-                    System.out.printf("Браузер: %s, Доля: %.2f%%%n", browser, (Double) share * 100));
-           // statistics.missingPage.forEach(() ->
-           //         System.out.print("Браузер: %s, Доля: %.2f%%%n", browser, (Double) share * 100));
-            System.out.println(statistics.getMissingPage());
+            System.out.println(output);
         }
+
 
     }
 
+    public static String getStringFromStream(Stream<String> stringStream) {
+        return stringStream.collect(Collectors.joining(" "));
 
+    }
 
 }
 
